d58b100df9feca504db6c46038e11dfc6f5f316c
TwinbornPlate75 <42514046+TwinbornPlate75@users.noreply.github.com>
2023-11-28 21:42:06 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,18 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=TPK by TwinbornPlate75
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=raphael
+device.name2=
+device.name3=
+device.name4=
 device.name5=
-supported.versions=
+supported.versions=13-14
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
