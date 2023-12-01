977b96bda88720f71e71f82351de072a55b22ad0
TwinbornPlate75 <42514046+TwinbornPlate75@users.noreply.github.com>
2023-12-01 19:22:10 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,31 +1,31 @@
-### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
 
-### AnyKernel setup
-# global properties
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string=TPK by TwinbornPlate75
-do.devicecheck=1
+kernel.string=TPKernel by TwinbornPlate75
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=raphael
-supported.versions=13-14
 supported.patchlevels=
-supported.vendorpatchlevels=
 '; } # end properties
 
-# boot shell variables
+# shell variables
+block=auto;
 is_slot_device=auto;
 ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-# vendor_kernel_boot install
-split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
+
+## AnyKernel install
+split_boot;
 
 mv $home/rd-new.cpio $home/ramdisk-new.cpio
 
