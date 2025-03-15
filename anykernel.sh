fe87e22368c40ea4c62a5987181b4243c9d5cf6f
TwinbornPlate75 <42514046+TwinbornPlate75@users.noreply.github.com>
2025-03-15 18:19:36 +0800

    Adapt for oplus7p

--- anykernel.sh
+++ anykernel.sh
@@ -4,19 +4,28 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=MIUICX Kernel by TwinbornPlate75 @ CoolAPK
+kernel.string=Starfish Kernel by TwinbornPlate75 @ CoolAPK
 do.devicecheck=1
 do.cleanup=1
-device.name1=raphael
-device.name2=raphaelin
-device.name3=cepheus
+device.name1=OnePlus7
+device.name2=guacamoleb
+device.name3=OnePlus7Pro
+device.name4=guacamole
+device.name5=OnePlus7ProTMO
+device.name6=guacamolet
+device.name7=OnePlus7T
+device.name8=hotdogb
+device.name9=OnePlus7TPro
+device.name10=hotdog
+device.name11=OnePlus7TProNR
+device.name12=hotdogg
 supported.versions=11-15
 '; } # end properties
 
 ### AnyKernel install
 # boot shell variables
 BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=0;
+IS_SLOT_DEVICE=1;
 NO_BLOCK_DISPLAY=1;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
