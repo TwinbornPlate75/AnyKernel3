6594148b9a39f3f36cee27ab7871664528464598
TwinbornPlate75 <42514046+TwinbornPlate75@users.noreply.github.com>
2025-03-23 16:09:19 +0800

    clean install script

--- anykernel.sh
+++ anykernel.sh
@@ -5,48 +5,25 @@
 # global properties
 properties() { '
 kernel.string=Starfish Kernel by TwinbornPlate75 @ CoolAPK
-do.devicecheck=1
+do.devicecheck=0
 do.cleanup=1
-device.name1=alpha
-device.name2=alphaplus
+device.name1=
+device.name2=
 supported.versions=11-15
 '; } # end properties
 
 ### AnyKernel install
 # boot shell variables
 BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=auto;
+IS_SLOT_DEVICE=1;
 NO_BLOCK_DISPLAY=1;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-# patch dtb if using retrofit dynamic partitions
-grep -q "logical" /vendor/etc/fstab.qcom
-if [ $? -eq 0 ]; then
-    ui_print " " "Retrofit dynamic partitions detected. Patching dtb...";
-    fdtput -t s $AKHOME/dtb /firmware/android/boot_devices "soc/1d84000.ufshc";
-    fdtput -d $AKHOME/dtb /firmware/android/shared_meta;
-    fdtput -d $AKHOME/dtb /firmware/android/android_q_fstab;
-
-    DSP=true;
-else
-    # patch dtb if using erofs on /vendor
-    fs_type=$($AKHOME/tools/busybox mount | grep ' /vendor ' | awk '{print $5}');
-    if [ "$fs_type" = "erofs" ]; then
-        ui_print " " "EROFS filesystem type on /vendor detected. Patching dtb...";
-        fdtput $AKHOME/dtb /firmware/android/android_q_fstab/vendor type "erofs";
-        fdtput $AKHOME/dtb /firmware/android/android_q_fstab/vendor mnt_flags "ro";
-    fi;
-fi;
-
 # boot install
 split_boot;
-if [ "$DSP" = "true" ]; then
-    patch_cmdline "using_dynamic_partitions" "using_dynamic_partitions";
-fi;
 flash_boot;
-flash_dtbo;
 ## end boot install
 
 # cache clean
