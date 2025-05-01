0caf01baab0861ad186e9d617326960167442617
TwinbornPlate75 <3342733415@qq.com>
2025-05-01 15:55:25 +0800

    Remove DSP check

--- anykernel.sh
+++ anykernel.sh
@@ -32,30 +32,8 @@ RAMDISK_COMPRESSION=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-# patch dtb if using retrofit dynamic partitions
-grep -q "logical" /vendor/etc/fstab.qcom
-if [ $? -eq 0 ]; then
-    ui_print " " "Retrofit dynamic partitions detected. Patching dtb...";
-    fdtput -t s $AKHOME/dtb /firmware/android/boot_devices "soc/1d84000.ufshc";
-    fdtput -d $AKHOME/dtb /firmware/android/shared_meta;
-    fdtput -d $AKHOME/dtb /firmware/android/android_q_fstab;
-
-    DSP=true;
-else
-    # patch dtb if using erofs on /vendor
-    fs_type=$($AKHOME/tools/busybox mount | grep ' /vendor ' | awk '{print $5}');
-    if [ "$fs_type" = "erofs" ]; then
-        ui_print " " "EROFS filesystem type on /vendor detected. Patching dtb...";
-        fdtput $AKHOME/dtb /firmware/android/android_q_fstab/vendor type "erofs";
-        fdtput $AKHOME/dtb /firmware/android/android_q_fstab/vendor mnt_flags "ro";
-    fi;
-fi;
-
 # boot install
 split_boot;
-if [ "$DSP" = "true" ]; then
-    patch_cmdline "using_dynamic_partitions" "using_dynamic_partitions";
-fi;
 flash_boot;
 flash_dtbo;
 ## end boot install
