e9495734237beb9e0955bc0ab539765d5ec65777
TwinbornPlate75 <3342733415@qq.com>
2025-12-04 17:00:31 +0800

    auto libhwui inject offset analysis

--- anykernel.sh
+++ anykernel.sh
@@ -21,8 +21,27 @@ NO_BLOCK_DISPLAY=1;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
+# import inject functions
+. tools/inject.sh;
+
 # boot install
 split_boot;
+
+ui_print " " "Analyzing libhwui.so for injection points...";
+INJECTION_OFFSETS=""
+if [ -f "/system/lib64/libhwui.so" ]; then
+    INJECTION_OFFSETS=$(analyze_libhwui "/system/lib64/libhwui.so" 2>/dev/null)
+    if [ $? -eq 0 ] && [ -n "$INJECTION_OFFSETS" ]; then
+        ui_print " " "Injection points found: $INJECTION_OFFSETS";
+        INJECT1=$(echo "$INJECTION_OFFSETS" | cut -d' ' -f1)
+        INJECT2=$(echo "$INJECTION_OFFSETS" | cut -d' ' -f2)
+        patch_cmdline "hwui_inject1" "hwui_inject1=$INJECT1";
+        patch_cmdline "hwui_inject2" "hwui_inject2=$INJECT2";
+    else
+       ui_print " " "Failed to analyze libhwui.so";
+    fi
+fi
+
 flash_boot;
 ## end boot install
 
