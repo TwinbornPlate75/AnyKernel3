ad521a8fc55c5797eafff31ccd0345ca0433f333
TwinbornPlate75 <3342733415@qq.com>
2026-01-23 01:46:51 +0800

    Fix dtb patch feature

--- anykernel.sh
+++ anykernel.sh
@@ -30,38 +30,38 @@ RAMDISK_COMPRESSION=auto;
 split_boot;
 
 ui_print " " "Analyzing libhwui.so for injection points...";
-INJECTION_OFFSETS=""
 if [ -f "/system/lib64/libhwui.so" ]; then
     INJECTION_OFFSETS=$(analyze_libhwui "/system/lib64/libhwui.so" 2>/dev/null)
     if [ $? -eq 0 ] && [ -n "$INJECTION_OFFSETS" ]; then
         ui_print " " "Injection points found: $INJECTION_OFFSETS";
-        INJECT1=$(echo "$INJECTION_OFFSETS" | cut -d' ' -f1)
-        INJECT2=$(echo "$INJECTION_OFFSETS" | cut -d' ' -f2)
+        INJECT1=$(echo "$INJECTION_OFFSETS" | cut -d' ' -f1);
+        INJECT2=$(echo "$INJECTION_OFFSETS" | cut -d' ' -f2);
         patch_cmdline "hwui_inject1" "hwui_inject1=$INJECT1";
         patch_cmdline "hwui_inject2" "hwui_inject2=$INJECT2";
     else
        ui_print " " "Failed to analyze libhwui.so";
-    fi
-fi
+    fi;
+fi;
 
 # patch dtb if using retrofit dynamic partitions
-grep -q "logical" /vendor/etc/fstab.qcom
+grep -q "logical" /vendor/etc/fstab.qcom;
 if [ $? -eq 0 ]; then
     ui_print " " "Retrofit dynamic partitions detected. Patching dtb...";
-    fdtput -t s $AKHOME/dtb /firmware/android/boot_devices "soc/1d84000.ufshc";
-    fdtput -d $AKHOME/dtb /firmware/android/shared_meta;
-    fdtput -d $AKHOME/dtb /firmware/android/android_q_fstab;
-
     patch_cmdline "using_dynamic_partitions" "using_dynamic_partitions";
+    fdtput -ts $AKHOME/dtb /firmware/android boot_devices "soc/1d84000.ufshc";
+    fdtput -r $AKHOME/dtb /firmware/android/vbmeta;
+    fdtput -r $AKHOME/dtb /firmware/android/fstab;
 else
     # patch dtb if using erofs on /vendor
-    fs_type=$($AKHOME/tools/busybox mount | grep ' /vendor ' | awk '{print $5}');
+    fs_type=$(mount | grep ' /vendor ' | awk '{print $5}');
     if [ "$fs_type" = "erofs" ]; then
         ui_print " " "EROFS filesystem type on /vendor detected. Patching dtb...";
-        fdtput $AKHOME/dtb /firmware/android/android_q_fstab/vendor type "erofs";
-        fdtput $AKHOME/dtb /firmware/android/android_q_fstab/vendor mnt_flags "ro";
-    fi
-fi
+        fdtput -ts $AKHOME/dtb /firmware/android/fstab/vendor type "erofs";
+        fdtput -ts $AKHOME/dtb /firmware/android/fstab/vendor mnt_flags "ro";
+    fi;
+fi;
+cat $AKHOME/Image.gz $AKHOME/dtb > $AKHOME/Image.gz-dtb;
+rm -rf $AKHOME/Image.gz $AKHOME/dtb;
 
 flash_boot;
 flash_dtbo;
